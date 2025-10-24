fc99d690b9a6ccbb31c8704a91ff5b04a998b05a
Anu-62anu <120464182+Anu-62anu@users.noreply.github.com>
2025-10-24 11:57:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { createApp } = require('./src/app');
 const { initializeSDK } = require('./src/config');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3200;
 
 (async () => {
   try {
