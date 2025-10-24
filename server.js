e31de8b8337ac8ac21aca526e23dc5da65983acc
Anu-62anu <120464182+Anu-62anu@users.noreply.github.com>
2025-10-24 12:09:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3200;
     }
     const app = createApp(sdk);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
